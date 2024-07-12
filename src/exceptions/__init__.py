74bb7bbae45b3bb2ada782cb0972f573c9aeca4d
Noam Abubtul <135932038+NoamAbutbul@users.noreply.github.com>
2024-07-12 14:34:57 +0300

    Revert "Base crud functions"

--- src/exceptions/__init__.py
+++
@@ -1,2 +0,0 @@
-"""this file made to make exception to a package"""
-from src.exceptions.base_exception import BaseException
--- src/exceptions/base_exception/__init__.py
+++
@@ -1,2 +0,0 @@
-"""this file made to make base_exception to a package"""
-from src.exceptions.base_exception.base_exception import BaseException
